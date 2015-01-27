7dea0b704ef50fc99b07aa100b8efb4ce71df3c3
Edmagne Lira dos Santos <edmagne@yahoo.com.br>
2015-01-27 14:28:37 -0200

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,3 @@ license          'All rights reserved'
 description      'Installs/Configures sensu on centos'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-
-depends "tar-cookbook"
